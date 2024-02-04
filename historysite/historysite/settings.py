5a7f89dd4aa1b36125d0f9fae7027d82d28f528c
Djamper(Alexsey Kuznetsov) <DjamperKuz@yandex.ru>
2024-02-04 16:32:13 +0300

    Изменил файл настроек приложения добавив подключение ключа с помощью переменных окружения, обновил gitignore и requirements.txt

--- historysite/historysite/settings.py
+++ historysite/historysite/settings.py
@@ -11,17 +11,25 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-from .secrets_key import SECRET_KEY
+import os
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
+# Start Django-environ
+env = environ.Env(
+    DEBUG=(bool, False)
+)
+# reading .env file
+env.read_env(os.path.join(BASE_DIR, '.env'))
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = SECRET_KEY
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- historysite/runserver.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-call D:\Django_history\venv\Scripts\activate
-python manage.py runserver
